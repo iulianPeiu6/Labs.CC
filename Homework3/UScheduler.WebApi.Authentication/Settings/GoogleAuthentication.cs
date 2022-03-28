1754d000d2eea240d144585b4e48074b1ac90c12
Vlad Ciubotariu <vlad.ciubotariu@centric.eu>
2022-03-28 22:37:30 +0300

    Added google authentification

---
+++ Homework3/UScheduler.WebApi.Authentication/Settings/GoogleAuthentication.cs
@@ -0,0 +1,8 @@
+﻿namespace UScheduler.WebApi.Authentication.Settings
+{
+    internal sealed class GoogleAuthentication
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
