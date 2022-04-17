c0736f5d03c385c433ed0af5a4846db40d73cac3
Iulian Peiu <iulianpeiu6@gmail.com>
2022-04-17 20:57:46 +0300

    Update Workspace API

--- Homework4/UScheduler.WebApi/UScheduler.WebApi.Workspaces/Controllers/v1/WorkspacesController.cs
+++ Homework4/UScheduler.WebApi/UScheduler.WebApi.Workspaces/Controllers/v1/WorkspacesController.cs
@@ -29,10 +29,10 @@ namespace UScheduler.WebApi.Workspaces.Controllers.v1
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetWorkspacesByOwnerIdAsync([FromQuery] Guid ownerId)
+        public async Task<IActionResult> GetWorkspacesByOwnerAsync([FromQuery] string owner)
         {
-            logger?.LogDebug($"Handeling GET request on api/v1/Workspaces?ownerId={ownerId}");
-            var result = await provider.GetOwnerWorkspacesAsync(ownerId);
+            logger?.LogDebug($"Handeling GET request on api/v1/Workspaces?owner={owner}");
+            var result = await provider.GetOwnerWorkspacesAsync(owner);
 
             if (result.IsSuccess)
             {
@@ -43,9 +43,12 @@ namespace UScheduler.WebApi.Workspaces.Controllers.v1
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateWorkspaceAsync([FromBody] CreateWorkspaceModel workspace)
+        public async Task<IActionResult> CreateWorkspaceAsync([FromBody] CreateWorkspaceModel workspace, [FromHeader] string createdBy)
         {
             logger?.LogDebug("Handeling POST request on api/v1/Workspaces");
+
+            workspace.CreatedBy = createdBy;
+
             var result = await provider.CreateWorkspaceAsync(workspace);
 
             if (result.IsSuccess)
@@ -57,9 +60,12 @@ namespace UScheduler.WebApi.Workspaces.Controllers.v1
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateAsync(Guid id, [FromBody] UpdateWorkspaceModel workspace)
+        public async Task<IActionResult> UpdateAsync(Guid id, [FromBody] UpdateWorkspaceModel workspace, [FromHeader] string updatedBy)
         {
             logger?.LogDebug($"Handeling PUT request on api/v1/Workspaces/{id}");
+
+            workspace.UpdatedBy = updatedBy;
+
             var result = await provider.FullUpdateWorkspaceAsync(id, workspace);
 
             if (result.IsSuccess)
@@ -82,7 +88,12 @@ namespace UScheduler.WebApi.Workspaces.Controllers.v1
 
             if (patchDoc != null)
             {
-                var result = await provider.PartiallyUpdateWorkspaceAsync(id, patchDoc);
+                Request.Headers.TryGetValue("UpdatedBy", out var updatedBy);
+                if (string.IsNullOrEmpty(updatedBy))
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, new { message = ErrorMessage.UpdatedByHeaderValueIsMissing });
+                }
+                var result = await provider.PartiallyUpdateWorkspaceAsync(id, patchDoc, updatedBy);
 
                 if (result.IsSuccess)
                 {
